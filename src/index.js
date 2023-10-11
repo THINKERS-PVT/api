aa22678bbb489a38c64db9eb1c6ecb054360b076
THINKERS-PVT <147609967+THINKERS-PVT@users.noreply.github.com>
2023-10-11 17:10:09 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ const { v4: uuidv4 } = require('uuid');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const v1Router = require("./v1/routes");
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
+// const supabaseUrl = process.env.SUPABASE_URL;
+// const supabaseKey = process.env.SUPABASE_KEY;
+// const supabase = createClient(supabaseUrl, supabaseKey);
 
 app.use(cors()); // Enable CORS for all origins
 app.use(bodyParser.json({ limit: '1mb' }));
@@ -33,7 +33,7 @@ app.use("*", async (req, res) => {
 
   };
   
-  const { data: log, error } = await supabase.from('db_test').insert(data); 
+  // const { data: log, error } = await supabase.from('db_test').insert(data); 
 
   if (error) {
     console.error('Error inserting log:', error);
@@ -43,15 +43,16 @@ app.use("*", async (req, res) => {
     console.log('Log inserted successfully:', log);
     // res.send(data);
    
-    const _message=['Are you looking to enhance customer support, streamline operations, or boost engagement on your website? Look no further! Our chatbot is here to revolutionize the way you interact with your customers.',
-                    ' Powered by advanced artificial intelligence, our chatbot is designed to provide instant and accurate responses to customer queries, 24/7. Say goodbye to long wait times and hello to instant assistance!',
-                    'With our chatbot, you can automate repetitive tasks, such as answering frequently asked questions, processing orders, and providing personalized recommendations. This not only saves time and resources but also ensures consistent and efficient customer service.',
-                    'Moreover, our chatbot is highly customizable, allowing you to tailor its personality and responses to align with your brand voice and values. It seamlessly integrates with various platforms, including websites, messaging apps, and social media channels, providing a unified and convenient customer experience.',
-                    'Stay ahead of the competition and deliver exceptional customer service with our cutting-edge chatbot product. Experience the power of AI-driven automation and witness increased customer satisfaction, improved efficiency, and accelerated business growth.',
-                    'Ready to transform the way you engage with your customers? Try our chatbot today and unlock a world of possibilities!'];
-     const rand_index = Math.floor(Math.random() * _message.length);
+    // const _message=['Are you looking to enhance customer support, streamline operations, or boost engagement on your website? Look no further! Our chatbot is here to revolutionize the way you interact with your customers.',
+    //                 ' Powered by advanced artificial intelligence, our chatbot is designed to provide instant and accurate responses to customer queries, 24/7. Say goodbye to long wait times and hello to instant assistance!',
+    //                 'With our chatbot, you can automate repetitive tasks, such as answering frequently asked questions, processing orders, and providing personalized recommendations. This not only saves time and resources but also ensures consistent and efficient customer service.',
+    //                 'Moreover, our chatbot is highly customizable, allowing you to tailor its personality and responses to align with your brand voice and values. It seamlessly integrates with various platforms, including websites, messaging apps, and social media channels, providing a unified and convenient customer experience.',
+    //                 'Stay ahead of the competition and deliver exceptional customer service with our cutting-edge chatbot product. Experience the power of AI-driven automation and witness increased customer satisfaction, improved efficiency, and accelerated business growth.',
+    //                 'Ready to transform the way you engage with your customers? Try our chatbot today and unlock a world of possibilities!'];
+    //  const rand_index = Math.floor(Math.random() * _message.length);
     
-    res.send('<br> Subscribe to Our Chatbot to use it ... <br>'+_message[rand_index]);
+    // res.send('<br> Subscribe to Our Chatbot to use it ... <br>'+_message[rand_index]);
+    res.send(data);
   }
 });
 
